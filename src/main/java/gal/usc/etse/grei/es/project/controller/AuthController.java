5f09520144442f97a811f9489093b81c9874e3ae
Oscar <panecillopeter@gmail.com>
2023-11-28 16:37:38 +0100

    Terminado toda la 3 entrega de documentacion

--- src/main/java/gal/usc/etse/grei/es/project/controller/AuthController.java
+++ src/main/java/gal/usc/etse/grei/es/project/controller/AuthController.java
@@ -1,10 +1,14 @@
 package gal.usc.etse.grei.es.project.controller;
 
 
+import gal.usc.etse.grei.es.project.model.Assessment;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,6 +27,23 @@ public class AuthController {
             summary = "Login",
             description = "Login with username and password to obtain a JWT token."
     )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Login successful",
+                    content = @Content(
+                            mediaType = "application/json",
+                            examples = @ExampleObject(
+                                    value = "{\"email\": \"test@test.com\", \"password\": \"test\"}"
+                            )
+                    )
+            ),
+           @ApiResponse(
+                    responseCode = "401",
+                    description = "Unauthorized",
+                    content = @Content
+            )
+    })
     public void login(
             @io.swagger.v3.oas.annotations.parameters.RequestBody(
                     description = "User and password for authentication",
